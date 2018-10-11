2d801084548e2923d3232359d722befad9768873
Yves Jakob <yves.jakob@icf-passau.de>
2018-10-11 15:17:20 +0200

    Initial Commit

---
+++ Cinebox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cinebox
+//
+//  Created by Yves Jakob on 11/10/2018.
+//  Copyright © 2018 Yves Jakob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
